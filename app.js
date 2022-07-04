60e0559e59c40349ae9e17e996eb0642dd2625b1
Waro <waro.brambilla@gmail.com>
2022-07-04 13:15:56 +0000

    commit message

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 window.onload = function (){
+       
     var cardValue = ["A","2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
     var suit = ["♥", "♦", "♣", "♠"]
     var rndcardValue = cardValue[Math.floor(Math.random() * cardValue.length)];
     var rdnSuit = suit[Math.floor(Math.random() * suit.length)];
     
+        document.getElementById('top-suit').innerHTML = rdnSuit;
+    document.getElementById('bottom-suit').innerHTML = rdnSuit;
+    document.getElementById('cardnumber').innerHTML = rndcardValue;
+
     return rdnSuit, rndcardValue;
-    
-    document.queryselector(".seed").innerHTML= rdnSuit;
-    document.queryselector(".cardnumber").innerHTML= rndcardValue;
-
-
-
-
 }
     
